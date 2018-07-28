9fcbf0252a1680cca1e0a8403ac2d4fba4d4e832
Edgar Santos-Fernandez <edgar.santosfdez@gmail.com>
2018-07-28 14:20:08 +0000

    version 0.0.1

---
+++ R/ActisoftR.r
@@ -0,0 +1,16 @@
+#' ActisoftR: A toolbox for parsing scored actigraphy data.
+#'
+#'
+#' @docType package
+#' @name ActisoftR
+#' @useDynLib ActisoftR
+#' @importFrom dplyr tbl_df filter mutate select_ rename group_by slice
+#' @importFrom data.table rbindlist
+#' @importFrom tibble add_column
+#' @importFrom magrittr %>%
+#' @importFrom utils read.csv
+#' @importFrom lubridate hours days
+#' @importFrom stats setNames update
+#' @importFrom scales date_breaks date_format
+
+
